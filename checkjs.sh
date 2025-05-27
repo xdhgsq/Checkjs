2e3df3513a9c90c337c8e1b2334dedcbc5e8b058
ITdesk <you@example.com>
2025-05-27 22:29:35 +0800

    更新变量
    Signed-off-by: ITdesk <you@example.com>

--- checkjs.sh
+++ checkjs.sh
@@ -619,7 +619,7 @@ description_if() {
 }
 
 task() {
-	cron_version="3.5"
+	cron_version="3.6"
 	if [ `grep -o "Checkjs的定时任务$cron_version" $cron_file |wc -l` = "0" ]; then
 		echo "不存在计划任务开始设置"
 		task_delete
@@ -633,7 +633,7 @@ task() {
 task_add() {
 cat >>$cron_file <<EOF
 #**********这里是Checkjs的定时任务$cron_version版本**********#102#
-*/5 * * * * $cron_user source /etc/profile && $dir_file/checkjs.sh >/tmp/checkjs.log 2>&1 #102#
+* */1 * * * $cron_user $dir_file/checkjs.sh >/tmp/checkjs.log 2>&1 #102#
 45 21 * * * $cron_user $dir_file/checkjs.sh update_script  >/tmp/checkjs_update_script.log 2>&1 #102#
 45 23 * * * $cron_user $dir_file/checkjs.sh rm_log 2>&1 #102#
 ###################请将其他定时任务放到底下#########102#
